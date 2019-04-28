7e0cc7033316710b13b40676de21d30ae829d218
Paul Gamble <pgamble@gmail.com>
2019-04-28 15:34:42 -0400

    💄  - fixing up single header that passes props

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 const Header = (props) => (
+    
 
-    <header className="App-header">
-        {props.providerData && <p>{props.providerData[0].displayName}</p>}
+    <header className="App-header">      
+       
+        {props.providerData && <button
+            
+            aria-haspopup="true"
+            aria-expanded={"false"}
+            id="hamburger-menu" >
+            <i className="main-button-icon fa fa-bars fa-3x"></i>
+        </button>}
 
-        <h1>Habit Tracker</h1>
+            <h1>Habit Tracker</h1> 
+        
+        
+        {props.providerData && 
+               
+                        <img src={props.providerData[0].photoURL}
+                             alt={props.providerData[0].providerName}
+                             className="container__profile--photo" />
+                   
+               }
+            
     </header>
+   
 );
 
 export default Header;
