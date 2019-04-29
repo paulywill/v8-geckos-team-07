03f685feb70c804b634756409ce4aaed33d0c79a
Paul Gamble <pgamble@gmail.com>
2019-04-28 21:09:10 -0400

    🔥  - removed firebase folder and old components
    The firebase folder contained redundent files and components that are no longer being used. #94

--- client/src/firebase/auth.js
+++
@@ -1,9 +0,0 @@
-import firebase from './firebase';
-
-export const getAuth = () => {
-  return firebase.auth();
-};
-
-export const googleOAuth = () => {
-  return new firebase.firebase_.auth.GoogleAuthProvider();
-};
